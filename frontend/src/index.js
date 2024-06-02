58a2c04e8cf11a7858f29b895ce28d27d413ab17
Eng-Liban <LibanAbdulahi0008@gmail.com>
2024-06-02 15:08:58 +0300

    file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 export let token=localStorage.getItem('token')
-export let endpoint='https://authorization-1.onrender.com/api'
+export let endpoint='https://authorization-1.onrender.com/api/'
 const container = document.querySelector('#root')
 const root = createRoot(container)
 root.render(
